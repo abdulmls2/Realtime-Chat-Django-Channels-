470359b95f6f509e8cbb7ed7a369971a19d453e4
abdulmls2 <r0932052@student.thomasmore.be>
2023-08-28 23:14:53 +0200

    timezone update

--- room/admin.py
+++ room/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from .models import *
 
+
+class TimeFieldAdmin(admin.ModelAdmin):
+    readonly_fields = ('date_added',)
+
+
 admin.site.register(Room)
-admin.site.register(Message)
+admin.site.register(Message, TimeFieldAdmin)
--- room/models.py
+++ room/models.py
@@ -2,25 +2,6 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-# class Room(models.Model):
-#     # creator = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
-#     name = models.CharField(max_length=255)
-#     slug = models.SlugField(unique=True)
-#     # participants = models.ManyToManyField(User, related_name='joined_rooms')
-#
-#     def __str__(self):
-#         return self.name
-#
-#
-# class Message(models.Model):
-#     room = models.ForeignKey(Room, related_name='messages', on_delete=models.CASCADE)
-#     user = models.ForeignKey(User, related_name='messages', on_delete=models.CASCADE)
-#     content = models.TextField()
-#     date_added = models.DateTimeField(auto_now_add=True)
-#
-#     class Meta:
-#         ordering = ('date_added',)
-
 class Room(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
